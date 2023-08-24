f99dfb5155762a9010912dce9b985aaa8f0375a1
enahha <dmsdk921@gmail.com>
2023-08-24 09:29:29 +0900

    def summary

---
+++ study0823/src/study0823/Summary/MainSummary_class.java
@@ -0,0 +1,11 @@
+package study0823.Summary;
+
+public class MainSummary_class {
+	public static void main(String[] args) {
+		
+		// 클래스
+		//7.1 데이터 타입
+		// 		public class 클래스명{}
+		// 7.2 클래스 구성 요소 : 필드영역, 생성자 영역(기본생성자,인자 있는 생성자), 메소드 영역(getters/setters, 정적, 동적)
+	}
+}
